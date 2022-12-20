f8fce82f80a1fdef570c2892b2d97b8dadef112d
gustavo <gustavo.isaac@gmail.com>
2022-12-20 16:24:12 +0000

    Composables for content

--- app/src/main/java/com/artemissoftware/amphitriteui2/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui2/MainActivity.kt
@@ -3,11 +3,14 @@ package com.artemissoftware.amphitriteui2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.artemissoftware.amphitriteui2.ui.theme.AmphitriteUI2Theme
@@ -22,7 +25,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+
+
+
+                    SingleContent {
+
+                    }
+
+
+
                 }
             }
         }
@@ -30,14 +42,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SingleContent(
+    content: @Composable() () -> Unit
+){
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AmphitriteUI2Theme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        content()
     }
+
 }
