934d4e0a88307d7d1c22aa93073ddd8a13dd568d
gustavo <gustavo.isaac@gmail.com>
2023-01-07 18:10:47 +0000

    categoriestable - CategoryBox

---
+++ app/src/main/java/com/artemissoftware/amphitriteui2/categoriestable/composables/CategoryBox.kt
@@ -0,0 +1,39 @@
+package com.artemissoftware.amphitriteui2.categoriestable.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.amphitriteui2.categoriestable.models.Category
+
+@Composable
+fun CategoryBox(
+    item: Category,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = item.label,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MonthBoxPreview() {
+    CategoryBox(Category.mock)
+}
--- app/src/main/java/com/artemissoftware/amphitriteui2/categoriestable/models/Category.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui2/categoriestable/models/Category.kt
@@ -3,4 +3,12 @@ package com.artemissoftware.amphitriteui2.categoriestable.models
 data class Category(
     val label: String,
     val percentage: Int, // 0..+100
-)
+){
+
+    companion object{
+
+        val mock = Category(label = "My category", percentage = 10)
+
+    }
+
+}
