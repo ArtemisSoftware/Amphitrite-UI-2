e9234205cfeea6710009bebfe41bc15fbf4497e7
gustavo <gustavo.isaac@gmail.com>
2023-07-11 15:05:06 +0100

    dragdrop - Person

---
+++ app/src/main/java/com/artemissoftware/amphitriteui2/dragdrop/models/Person.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.amphitriteui2.dragdrop.models
+
+import androidx.compose.ui.graphics.Color
+
+data class Person(
+    val name: String,
+    val id: String,
+    val backgroundColor: Color,
+)
