000b9d7d3272ab6bc01f808944cc420f9fca613e
gustavo <gustavo.isaac@gmail.com>
2024-01-02 08:56:00 +0000

    bottleCap

---
+++ app/src/main/java/com/artemissoftware/amphitriteui2/animations/waterbottle/BottleCap.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.amphitriteui2.animations.waterbottle
+
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+
+fun DrawScope.bottleCap(
+    capColor: Color,
+    capWidth: Float,
+    capHeight: Float,
+) {
+    drawRoundRect(
+        color = capColor,
+        size = Size(capWidth, capHeight),
+        topLeft = Offset(size.width / 2 - capWidth / 2f, 0f),
+        cornerRadius = CornerRadius(45f, 45f),
+    )
+}
