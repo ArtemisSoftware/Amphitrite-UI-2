955dda7299a0ef02338b3e2154df13cf976ca23b
gustavo <gustavo.isaac@gmail.com>
2024-03-10 11:51:00 +0000

    CardScan extra config

--- app/src/main/java/com/artemissoftware/amphitriteui2/Amphitriteui2Application.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui2/Amphitriteui2Application.kt
@@ -1,7 +1,30 @@
 package com.artemissoftware.amphitriteui2
 
 import android.app.Application
+import com.getbouncer.cardscan.ui.CardScanActivity
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Amphitriteui2Application : Application()
+class Amphitriteui2Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate();
+
+        /*
+         * CardScan will attempt to update the ML models used to scan payment
+         * cards. By placing the call to the `warmUp` method in the
+         * `onApplicationCreate` method of your app, you allow the most time
+         * possible for models to upgrade. Note that `warmUp` processes on a
+         * background thread and will not affect your app's startup time.
+         *
+         * Note: the last parameter, `initializeNameAndExpiryExtraction`,
+         * determines if name and expiry extraction (beta feature) should be
+         * turned on.
+         */
+        CardScanActivity.warmUp(
+            context = this,
+            apiKey = API_KEY,
+            initializeNameAndExpiryExtraction = true
+        )
+    }
+}
--- app/src/main/java/com/artemissoftware/amphitriteui2/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.artemissoftware.amphitriteui2
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.animation.OvershootInterpolator
@@ -23,14 +24,23 @@ import com.artemissoftware.amphitriteui2.dragdrop.DragDropViewModel
 import com.artemissoftware.amphitriteui2.multiplescreens.rememberWindowSize
 import com.artemissoftware.amphitriteui2.stopwatch.StopWatchViewModel
 import com.artemissoftware.amphitriteui2.ui.theme.AmphitriteUI2Theme
+import com.getbouncer.cardscan.ui.CardScanActivity
+import com.getbouncer.cardscan.ui.CardScanActivityResult
+import com.getbouncer.cardscan.ui.CardScanActivityResultHandler
 import dagger.hilt.android.AndroidEntryPoint
+import io.card.payment.CardIOActivity
 
+val API_KEY = "qOJ_fF-WLDMbG05iBq5wvwiTNTmM2qIn"
+
+// val API_KEY = "51Or2cBG9KqZNCuLw4tGxLixAo2b7X3neTKKlCAdbBSNmHH6uiHjLbiOCrDSi3a0wzjfUgoRZgSMSaB6XGcAjnK0G00SkXGh3PS"
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), CardScanActivityResultHandler {
 
     private val dragDropViewModel = DragDropViewModel()
     private val stopWatchViewModel = StopWatchViewModel()
     private val mainViewModel by viewModels<MainViewModel>()
+
+    val MY_SCAN_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,6 +73,15 @@ class MainActivity : ComponentActivity() {
                 zoomY.start()
             }
         }
+
+        // --onCardIoScanPress()
+        // --creditCardOrc(this)
+//        CardScanActivity.warmUp(
+//            context = this,
+//            apiKey = API_KEY,
+//            initializeNameAndExpiryExtraction = true
+//        )
+
         setContent {
             val state = mainViewModel.state.collectAsState().value
             val navController = rememberNavController()
@@ -104,5 +123,64 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    fun creditCardOrc(mainActivity: MainActivity) {
+        CardScanActivity.start(
+            activity = mainActivity,
+            apiKey = API_KEY,
+            enableEnterCardManually = true,
+
+            // expiry extraction is in beta. See the comment below.
+            enableExpiryExtraction = true,
+
+            // name extraction is in beta. See the comment below.
+            enableNameExtraction = true,
+        )
+    }
+
+    fun onCardIoScanPress() {
+        val scanIntent = Intent(this, CardIOActivity::class.java)
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false) // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE)
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (CardScanActivity.isScanResult(requestCode)) {
+//            CardScanActivity.parseScanResult(resultCode, data, this)
+//        }
+//    }
+
+    override fun analyzerFailure(scanId: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun cameraError(scanId: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun canceledUnknown(scanId: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun cardScanned(scanId: String?, scanResult: CardScanActivityResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun enterManually(scanId: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun userCanceled(scanId: String?) {
+        TODO("Not yet implemented")
     }
 }
